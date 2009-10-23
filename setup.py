39d511ef079b599d257031f115f41371ef368dd9
Maurits van Rees <maurits@vanrees.org>
2009-10-23 15:16:31 +0000

    history, readme and version cleanup
    svn path=/Products.DataGridField/branches/1.7/; revision=11476

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.7 (svn/unreleased)'
+readme = open(os.path.join("Products", "DataGridField", "README.txt")).read()
+history = open(os.path.join("Products", "DataGridField", "HISTORY.txt")).read()
+version = open(os.path.join("Products", "DataGridField", "version.txt")).read()
+long_description = readme + "\n" + history
+
 
 setup(name='Products.DataGridField',
       version=version,
       description="A table input component for Plone.",
-      long_description=open(os.path.join("Products", "DataGridField", "README.txt")).read() + "\n" +
-                       open(os.path.join("Products", "DataGridField", "HISTORY.txt")).read(),
+      long_description=long_description,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
