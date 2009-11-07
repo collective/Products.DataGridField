5f93ee700d97f08ea5168bba67c18fd92f1dae93
Maurits van Rees <maurits@vanrees.org>
2009-11-07 01:44:00 +0000

    Preparing release 1.8a1
    svn path=/Products.DataGridField/trunk/; revision=11632

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.8dev'
+version = '1.8a1'
 readme = open(os.path.join("Products", "DataGridField", "README.txt")).read()
 history = open(os.path.join("Products", "DataGridField", "HISTORY.txt")).read()
 long_description = readme + "\n" + history
