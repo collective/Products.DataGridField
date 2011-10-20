4601b07fa49b895f5fc69fbca66505129077a04c
Erico Andrei <erico@simplesconsultoria.com.br>
2011-10-20 21:14:12 +0000

    Add isValidDate validator
    svn path=/Products.DataGridField/branches/lepri-datetime-column/; revision=13835

--- Products/DataGridField/validators.py
+++ Products/DataGridField/validators.py
@@ -5,10 +5,65 @@ always a hidden entry.  So we check if there is least one normal entry
 and one hidden entry, so more than 1 entry in total.
 """
 
+import time
+from DateTime.DateTime import DateTime
+from datetime import datetime
+
 from Products.validation import validation
 from Products.validation.interfaces.IValidator import IValidator
+
+from Products.DataGridField.DateTimeColumn import DateTimeColumn
+
 from zope.interface import implements
 
+from collective.js.jqueryui.utils import get_python_date_format
+
+class DateValidator:
+    """ Validate as True if the value is a valid date.
+    """
+
+    implements(IValidator)
+
+    def __init__(self, name, title='', description=''):
+        self.name = name
+        self.title = title or name
+        self.description = description
+
+    def date_columns(self,field):
+        '''Given a field we return a list of 
+           DateTimeColumns 
+        '''
+        columns = []
+        if not (field and hasattr(field,'columns')):
+            return columns
+        for column_id, column in field.widget.columns.items():
+            if isinstance(column, DateTimeColumn):
+                columns.append(column_id)
+        return columns
+
+    def __call__(self, value, *args, **kwargs):
+        request = kwargs.get('REQUEST',None)
+        field = kwargs.get('field', None)
+        date_columns = self.date_columns(field)
+        date_format = request and get_python_date_format(request) or '%Y/%m/%d'
+        for line in value:
+            for column in date_columns:
+                date = line[column]
+                if hasattr(date, 'strftime'):
+                    #Already a date
+                    continue
+                datestr = '%s 0:0:00' % (date)
+                try:
+                    tp = time.strptime(datestr, date_format + ' %H:%M:%S')
+                    DateTime(time.mktime(tp))
+                except ValueError:
+                    return ("%s is not a valid date." % date)
+        return True
+
+isValidDate = DateValidator(
+    'isValidDate', title='Valide DateTime entry',
+    description='The DateTime value must have be a valid date.')
+validation.register(isValidDate)
 
 class DataGridValidator:
     """Validate as True when having at least one DataGrid item.
