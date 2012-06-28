affb620b6db1b266bd06f705cf07128bbb4c52dc
Maurits van Rees <maurits@vanrees.org>
2012-06-28 11:36:51 +0200

    Back to development: 1.8.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.8.0'
+version = '1.8.1.dev0'
 readme = open(os.path.join("Products", "DataGridField", "README.txt")).read()
 history = open(os.path.join("Products", "DataGridField", "HISTORY.txt")).read()
 long_description = readme + "\n" + history
