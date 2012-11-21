d222ffc3f7f1ab2eed22dae84f3a8f3b267c2b1d
Peter Mathis <peter.mathis@kombinat.at>
2012-11-21 20:36:45 +0100

    Update Products/DataGridField/CheckboxColumn.py

--- Products/DataGridField/CheckboxColumn.py
+++ Products/DataGridField/CheckboxColumn.py
@@ -20,14 +20,6 @@ class CheckboxColumn(Column):
     """ Allow user select one from on/off options using check buttons.
 
     Implementation based on RadioColumn.
-
-    WARNING - Please note, the current implementation does not work when
-    some field on the form raises validation error. In this case all checkboxes
-    are cleared. Do not use CheckboxColumn if your form has some required fields
-    or validators active.
-
-    There are no on/off values. Widget view displays HTML character with code
-    &#10004; (checkmark) or -
     """
     security = ClassSecurityInfo()
 
